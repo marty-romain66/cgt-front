410f5c7b771a2f1023641a04e8b81cd6925e8404
Marty Romain <102585723+marty-romain66@users.noreply.github.com>
2022-09-07 14:23:58 +0200

    Add files via upload
    ok

--- src/componantes/Test.js
+++ src/componantes/Test.js
@@ -1,8 +1,8 @@
 import gsap from 'gsap';
 import React, {useEffect} from 'react';
-import SignInSide from './Article';
+
 import Header from './Header';
-import Test2 from './Test2';
+
 
 
 const Test = () => {
@@ -14,8 +14,9 @@ const Test = () => {
 
     return (
      <>
-    <Header/>
-    <SignInSide/>
+    {/* <Header/>
+    <SignInSide/> */}
+    fffffffffffffffffffffffffff
     </>
     );
 };
